717a185f9bcdf00fb14d4a325ab0077062037e20
robbinl <robbinl@telusplanet.net>
2024-03-07 12:56:19 -0700

    restful

--- r-restfulapi-local-storage/lib/d6/views/home.dart
+++ r-restfulapi-local-storage/lib/d6/views/home.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: avoid_print, use_key_in_widget_constructors, use_build_context_synchronously
 
 import 'package:flutter/material.dart';
-import 'dart:async';
 import 'package:robbinlaw/d6/services/restapi.dart';
 
 class HomeView extends StatefulWidget {
